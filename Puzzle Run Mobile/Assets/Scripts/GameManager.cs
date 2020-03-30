d32239da800242af6ab22c557710ff95309e6750
Héron Silva <heronsilva@poli.ufrj.br>
2020-03-30 09:34:09 -0300

    Add spawn wall method

--- Puzzle Run Mobile/Assets/Scripts/GameManager.cs
+++ Puzzle Run Mobile/Assets/Scripts/GameManager.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject _wallPrefab, _dynamicContainer;
+
+    private GameObject _wall;
+
+    private void Start()
+    {
+        SpawnWall();
+    }
+
+    private void SpawnWall()
+    {
+        _wall = Instantiate(_wallPrefab, _dynamicContainer.transform) as GameObject;
+    }
 }
