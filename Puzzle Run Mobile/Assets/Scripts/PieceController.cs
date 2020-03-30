d488f72260b009dda8a779f95df346bb2cb84689
Héron Silva <heronsilva@poli.ufrj.br>
2020-03-30 13:32:39 -0300

    Limit piece movement time

--- Puzzle Run Mobile/Assets/Scripts/PieceController.cs
+++ Puzzle Run Mobile/Assets/Scripts/PieceController.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class PieceController : MonoBehaviour
 {
+    [SerializeField] private float _moveDeltaTime = .1f;
+
     private FixedJoystick _joystick;
+    private float _lastMove;
 
     private void Start()
     {
         _joystick = FindObjectOfType<FixedJoystick>();
+        _lastMove = Time.time;
     }
 
     private void Update()
     {
-        this.transform.position += Vector3.right * _joystick.Horizontal;
-        this.transform.position += Vector3.up * _joystick.Vertical;
+        float h = _joystick.Horizontal;
+        float v = _joystick.Vertical;
+
+        if ((h != 0 || v != 0) && Time.time - _lastMove > _moveDeltaTime)
+        {
+            this.transform.position += Vector3.right * _joystick.Horizontal;
+            this.transform.position += Vector3.up * _joystick.Vertical;
+            _lastMove = Time.time;
+        }
+        
     }
 }
