9be474bc2487ee933adf9b95a38dff797db28f80
Héron Silva <heronsilva@poli.ufrj.br>
2020-03-30 22:12:21 -0300

    Add wall rotate x method

--- Puzzle Run Mobile/Assets/Scripts/WallAnimations.cs
+++ Puzzle Run Mobile/Assets/Scripts/WallAnimations.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class WallAnimations : MonoBehaviour
 {
+    private Animator _anim;
+    
+    private List<string> xAnimations = new List<string>() { "WallRotateX", "WallRotateX-" };
+
+    private void Start()
+    {
+        _anim = this.gameObject.GetComponent<Animator>();
+        _anim.SetFloat("Speed", GameManager.Instance.AnimationSpeed);
+    }
+    
     public void Fade()
     {
         foreach (Transform cube in this.transform)
         {
             cube.gameObject.GetComponent<CubeAnimations>().Fade();
         }
+    }
+
+    public void RotateX()
+    {
+        int i = Random.Range(0, xAnimations.Count);
+        StartCoroutine(PlayAnimation(xAnimations[i]));
+    }
+
+    IEnumerator PlayAnimation(string animation)
+    {
+        float delay = GameManager.Instance.AnimationDelay;
+        yield return new WaitForSeconds(delay);
+        _anim.Play(animation);
     }
 }
