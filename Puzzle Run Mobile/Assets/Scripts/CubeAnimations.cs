f7cd423998adf1091924d1bcc8d80cff806b6280
Héron Silva <heronsilva@poli.ufrj.br>
2020-03-30 21:15:33 -0300

    Add cube and wall fade animation

--- Puzzle Run Mobile/Assets/Scripts/CubeAnimations.cs
+++ Puzzle Run Mobile/Assets/Scripts/CubeAnimations.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class CubeAnimations : MonoBehaviour
 {
-    
+    public void Fade()
+    {
+        StartCoroutine(PlayAnimation("CubeFade"));
+    }
+
+    IEnumerator PlayAnimation(string name)
+    {
+        float delay = GameManager.Instance.AnimationDelay;
+        float speed = GameManager.Instance.AnimationSpeed;
+        
+        yield return new WaitForSeconds(delay);
+        
+        this.GetComponent<Animator>().SetFloat("Speed", speed);
+        this.GetComponent<Animator>().Play(name);
+    }
 }
--- Puzzle Run Mobile/Assets/Scripts/WallAnimations.cs
+++ Puzzle Run Mobile/Assets/Scripts/WallAnimations.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class WallAnimations : MonoBehaviour
 {
-    
+    public void Fade()
+    {
+        foreach (Transform cube in this.transform)
+        {
+            cube.gameObject.GetComponent<CubeAnimations>().Fade();
+        }
+    }
 }
