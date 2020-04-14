ad81dbc97506ff0ad4989c6aa98957224ce81c3e
Héron Silva <heronsilva@poli.ufrj.br>
2020-04-13 21:40:22 -0300

    Add gameover manager functionality

---
+++ Puzzle Run Mobile/Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public void TryAgain()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
