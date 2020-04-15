b769f715a3a6013f5a918543df0622cae471de80
Héron Silva <heronsilva@poli.ufrj.br>
2020-04-15 15:11:09 -0300

    Render game over data

--- Puzzle Run Mobile/Assets/Scripts/GameOverManager.cs
+++ Puzzle Run Mobile/Assets/Scripts/GameOverManager.cs
@@ -1,10 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverManager : MonoBehaviour
 {
+    [SerializeField] private Text timeText = null;
+    [SerializeField] private Text scoreText = null;
+    [SerializeField] private Text topScoreText = null;
+    [SerializeField] private Text crossesText = null;
+    [SerializeField] private Text topCrossesText = null;
+
+    private void Start()
+    {
+        UpdateGameOverUI();
+    }
+
+    private void UpdateGameOverUI()
+    {
+        TimeSpan time = TimeSpan.FromSeconds(PlayerPrefManager.GetLastTime());
+        string formatedTime = string.Format("{0:D2}:{1:D2}", time.Minutes, time.Seconds);
+        timeText.text = "TIME\n" + formatedTime;
+        scoreText.text = "SCORE\n" + PlayerPrefManager.GetLastScore();
+        topScoreText.text = "TOP SCORE\n" + PlayerPrefManager.GetTopScore();
+        crossesText.text = "CROSSES\n" + PlayerPrefManager.GetLastCrosses();
+        topCrossesText.text = "TOP CROSSES\n" + PlayerPrefManager.GetTopCrosses();
+    }
+
     public void TryAgain()
     {
         SceneManager.LoadScene("Game");
