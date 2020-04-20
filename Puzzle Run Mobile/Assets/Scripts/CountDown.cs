6ca66d8687b4bf95087290c25613375813f82cf3
Héron Silva <heronsilva@poli.ufrj.br>
2020-04-20 15:19:23 -0300

    Add count down animation sound

---
+++ Puzzle Run Mobile/Assets/Scripts/CountDown.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour
+{
+    [SerializeField] private AudioClip countDownBip = null;
+
+    private AudioSource audioSource;
+
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void CountDownBip()
+    {
+        audioSource.PlayOneShot(countDownBip);
+    }
+}
