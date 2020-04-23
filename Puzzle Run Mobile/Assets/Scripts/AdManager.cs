f63daafc4660aca12e86e0440fba17bc03c1845e
Héron Silva <heronsilva@poli.ufrj.br>
2020-04-23 09:51:27 -0300

    Add ad manager to game scene

---
+++ Puzzle Run Mobile/Assets/Scripts/AdManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdManager : MonoBehaviour
+{
+    [SerializeField] private string iOSGameID = "3568509";
+    [SerializeField] private string androidGameID = "3568508";
+
+    private string gameId;
+    private bool testMode = true;
+
+    private void Start()
+    {
+        #if UNITY_IOS
+            gameId = iOSGameID;
+        #endif
+        #if UNITY_ANDROID
+            gameId = androidGameID;
+        #endif
+
+        Advertisement.Initialize(gameId, testMode);
+    }
+}
