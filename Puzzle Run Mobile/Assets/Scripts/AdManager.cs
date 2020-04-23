4fbd3141eacb900843323ad86295f7aef6a66a1d
Héron Silva <heronsilva@poli.ufrj.br>
2020-04-23 10:31:36 -0300

    Add ad manager class

--- Puzzle Run Mobile/Assets/Scripts/AdManager.cs
+++ Puzzle Run Mobile/Assets/Scripts/AdManager.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
-public class AdManager : MonoBehaviour
+public class AdManager : MonoBehaviour, IUnityAdsListener
 {
     [SerializeField] private string iOSGameID = "3568509";
     [SerializeField] private string androidGameID = "3568508";
+    [SerializeField] private string nonRewardedPID = "video";
+    [SerializeField] private string rewardedPID = "rewardedVideo";
+    [SerializeField] private string bannerPID = "banner";
 
     private string gameId;
     private bool testMode = true;
@@ -20,6 +23,54 @@ public class AdManager : MonoBehaviour
             gameId = androidGameID;
         #endif
 
+        Advertisement.AddListener(this);
         Advertisement.Initialize(gameId, testMode);
     }
+
+    public IEnumerator ShowBannerAd()
+    {
+        while (! Advertisement.IsReady(bannerPID))
+            yield return null;
+
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        Advertisement.Banner.Show(bannerPID);
+    }
+
+    public void ShowNonRewardedAd()
+    {
+        StartCoroutine(ShowAd(nonRewardedPID));
+    }
+
+    public void ShowRewardedAd()
+    {
+        StartCoroutine(ShowAd(rewardedPID));
+    }
+
+    private IEnumerator ShowAd(string placementId)
+    {
+        while (! Advertisement.IsReady(placementId))
+            yield return null;
+
+        Advertisement.Show(placementId);
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if (placementId != rewardedPID) return;
+
+        switch (showResult)
+        {
+            case ShowResult.Finished:
+                // REWARD
+                break;
+            case ShowResult.Skipped:
+                break;
+            case ShowResult.Failed:
+                break;
+        }
+    }
+
+    public void OnUnityAdsDidStart(string placementId) {}
+    public void OnUnityAdsReady(string placementId) {}
+    public void OnUnityAdsDidError(string placementId) {}
 }
