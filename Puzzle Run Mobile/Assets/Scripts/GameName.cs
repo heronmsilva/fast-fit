7c4edf879ea22ef042c831f0950410937916aa16
Héron Silva <heronsilva@poli.ufrj.br>
2020-04-30 16:18:23 -0300

    Add game intro letter sounds

---
+++ Puzzle Run Mobile/Assets/Scripts/GameName.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameName : MonoBehaviour
+{
+    [SerializeField] private AudioClip letterEntry = null;
+    [SerializeField] private AudioSource audioSource = null;
+
+    private void LetterEntry()
+    {
+        audioSource.PlayOneShot(letterEntry);
+    }
+}
