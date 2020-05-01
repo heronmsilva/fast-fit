37241ae2de3c046806eb53b2d261f7c8471d41c9
Héron Silva <heronsilva@poli.ufrj.br>
2020-05-01 16:33:47 -0300

    Add records window texts translations

--- Fast Fit/Assets/Scripts/MenuTextManager.cs
+++ Fast Fit/Assets/Scripts/MenuTextManager.cs
@@ -6,6 +6,12 @@ using TMPro;
 public class MenuTextManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI tapToPlayText = null;
+    [SerializeField] private TextMeshProUGUI recordsTitleText = null;
+    [SerializeField] private TextMeshProUGUI topScoreText = null;
+    [SerializeField] private TextMeshProUGUI longestRunText = null;
+    [SerializeField] private TextMeshProUGUI topFitsInARunText = null;
+    [SerializeField] private TextMeshProUGUI topStreakText = null;
+    [SerializeField] private TextMeshProUGUI runsText = null;
 
     private void Awake() 
     {
@@ -20,6 +26,12 @@ public class MenuTextManager : MonoBehaviour
     public void UpdateTexts()
     {
         tapToPlayText.text = Texts.TapTopPlay;
+        recordsTitleText.text = Texts.Records;
+        topScoreText.text = Texts.TopScore;
+        longestRunText.text = Texts.LongestRun;
+        topFitsInARunText.text = Texts.TopFitsInARun;
+        topStreakText.text = Texts.TopStreak;
+        runsText.text = Texts.Runs;
     }
 
     private void SetupLanguage()
--- Fast Fit/Assets/Scripts/Texts.cs
+++ Fast Fit/Assets/Scripts/Texts.cs
@@ -23,4 +23,94 @@ public static class Texts
             }
         }
     }
+
+    public static string Records
+    {
+        get 
+        {
+            switch (Texts.Language)
+            {
+                case Texts.Portuguese:
+                    return "Recordes";
+                case Texts.English:
+                default:
+                    return "Records";
+            }
+        }
+    }
+
+    public static string TopScore
+    {
+        get 
+        {
+            switch (Texts.Language)
+            {
+                case Texts.Portuguese:
+                    return "Melhor Pontuação";
+                case Texts.English:
+                default:
+                    return "Top Score";
+            }
+        }
+    }
+
+    public static string LongestRun
+    {
+        get 
+        {
+            switch (Texts.Language)
+            {
+                case Texts.Portuguese:
+                    return "Corrida Mais Longa";
+                case Texts.English:
+                default:
+                    return "Longest Run";
+            }
+        }
+    }
+
+    public static string TopFitsInARun
+    {
+        get 
+        {
+            switch (Texts.Language)
+            {
+                case Texts.Portuguese:
+                    return "Quantidade de Acertos";
+                case Texts.English:
+                default:
+                    return "Top Fits in a Run";
+            }
+        }
+    }
+
+    public static string TopStreak
+    {
+        get 
+        {
+            switch (Texts.Language)
+            {
+                case Texts.Portuguese:
+                    return "Melhor Sequência";
+                case Texts.English:
+                default:
+                    return "Top Streak";
+            }
+        }
+    }
+
+    public static string Runs
+    {
+        get 
+        {
+            switch (Texts.Language)
+            {
+                case Texts.Portuguese:
+                    return "Corridas";
+                case Texts.English:
+                default:
+                    return "Runs";
+            }
+        }
+    }
 }
