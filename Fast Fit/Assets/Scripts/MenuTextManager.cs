9a0ed0f0d348ac96db40b7b7deaa09a2d1c85e5b
Héron Silva <heronsilva@poli.ufrj.br>
2020-05-01 16:01:55 -0300

    Implement initial language setup

---
+++ Fast Fit/Assets/Scripts/MenuTextManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuTextManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI tapToPlayText = null;
+
+    private void Awake() 
+    {
+        SetupLanguage();
+    }
+
+    private void Start()
+    {
+        UpdateTexts();
+    }
+
+    public void UpdateTexts()
+    {
+        tapToPlayText.text = Texts.TapTopPlay;
+    }
+
+    private void SetupLanguage()
+    {
+        switch (Application.systemLanguage)
+        {
+            case SystemLanguage.Portuguese:
+                Texts.Language = Texts.Portuguese;
+                break;
+            case SystemLanguage.English:
+            default:
+                Texts.Language = Texts.English;
+                break;
+        }
+    }
+}
---
+++ Fast Fit/Assets/Scripts/Texts.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Texts
+{
+    public static string Language = "en";
+
+    public const string English = "en";
+    public const string Portuguese = "pt";
+
+    public static string TapTopPlay
+    {
+        get 
+        {
+            switch (Texts.Language)
+            {
+                case Texts.Portuguese:
+                    return "Toque para Jogar";
+                case Texts.English:
+                default:
+                    return "Tap to Play";
+            }
+        }
+    }
+}
