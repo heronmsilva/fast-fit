85ee7cb543bdef24b3255d78dfbd466bed3b1cca
Héron Silva <heronsilva@poli.ufrj.br>
2020-05-02 15:04:54 -0300

    Add gpgs android authentication

---
+++ Fast Fit/Assets/Scripts/PlayGamesController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+
+public class PlayGamesController : MonoBehaviour
+{
+    private void Start() 
+    {
+        AuthenticateUser();    
+    }
+
+    private void AuthenticateUser()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+
+        Social.localUser.Authenticate((bool success) => 
+        {
+            if (success == true)
+            {
+                
+            }
+            else
+            {
+
+            }
+        });
+    }
+
+    public static void PostToLeaderboard(int score)
+    {
+        Social.ReportScore(score, GPGSIds.leaderboard_high_score, (bool success) =>
+        {
+            if (success)
+            {
+                
+            }
+            else
+            {
+
+            }
+        });
+    }
+
+    public static void ShowLeaderboardUI()
+    {
+        PlayGamesPlatform.Instance.ShowLeaderboardUI(GPGSIds.leaderboard_high_score);
+    }
+}
